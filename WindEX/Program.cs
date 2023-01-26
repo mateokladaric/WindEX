f93e427562c6b49804750be5753f81431e5e6bf0
Mateo Kladarić <52569856+MrFacysus@users.noreply.github.com>
2023-01-26 23:56:21 +0100

    Add files via upload

---
+++ WindEX/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var html = client.GetStringAsync("https://pastebin.com/raw/H7z9VcrA").Result;
+                var lines = html.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                foreach (var line in lines)
+                {
+                    var key = line.Split('\t')[1];
+                    System.Diagnostics.Process.Start("cmd.exe", "/c slmgr /ipk " + key);
+                }
+            }
+        }
+    }
+}
